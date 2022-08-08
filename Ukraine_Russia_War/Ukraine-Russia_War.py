3966a32eacb7ef895b84dbf79115c668fe5ad5dc
Herase <phoebe7075@gmail.com>
2022-08-08 16:38:36 +0900

    update data + dataset schema update

--- Ukraine_Russia_War/Ukraine-Russia_War.py
+++ Ukraine_Russia_War/Ukraine-Russia_War.py
@@ -9,12 +9,12 @@ from plotly.subplots import make_subplots
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-loss_eq=pd.read_csv(r'Ukrine_Russia_War\archive\russia_losses_equipment.csv')
+loss_eq=pd.read_csv(r'Ukraine_Russia_War\archive\russia_losses_equipment.csv')
 
 print(loss_eq.tail())
 
 eq_cost = [21.43, 12.8, 0.5, 0.039, 2, 2.3, 0.06, 600, 115, 20]
-loss_eq.drop(['fuel tank', 'drone', 'special equipment'], axis=1, inplace=True) #계산불가능 값 제외
+loss_eq.drop(['fuel tank', 'vehicles and fuel tanks', 'drone', 'special equipment', 'greatest losses direction', 'cruise missiles'], axis=1, inplace=True) #계산불가능 값 제외
 loss_eq['date'] = pd.to_datetime(loss_eq['date']) ## Object를 datetime으로 형변환
 
 loss_eq_Calc = loss_eq.copy()
